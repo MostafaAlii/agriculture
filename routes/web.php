70285d04d3f51a5886ea2e66d66df56000dd8045
wolf <ahmedragabyasin2020@gmail.com>
2022-03-04 02:32:10 +0200

    route from web to site

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\front;
-use App\Http\Livewire;
 
-// front routes
-route::get('/',[front\HomeController::class,'index'])->name('home');
-route::get('/aboutUs',[front\AboutUsController::class,'index'])->name('aboutUs');
-route::get('/shop',Livewire\front\shop::class)->name('shop');
-// Route::get('/product', function () {
-//     return view('front.product');
-// });
-// end front routes
 
 
 
