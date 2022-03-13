66449474e3674cbcb7f72827723e29113322b328
MostafaAlii <mostafa0alii@gmail.com>
2022-03-13 16:46:22 +0200

    fix settings

--- resources/lang/ar/Admin/setting.php
+++ resources/lang/ar/Admin/setting.php
@@ -1,41 +1,33 @@
 <?php
 return [
-    'page_title_in_sidebar' =>  'الاعدادات العامة',
-    'settingPageTitle'=>  'الاعدادات العامة',
-    'dashboard' => 'اللوحة الرئيسية',
-    'setting_info' => 'معلومات الإعدادات',
-    'settings' => 'الإعدادات',
-    'site_name' => 'إسم الموقع',
-    'supporting_mail' => 'البريد الإلكتروني',
-    'primary_phone' => 'رقم الهاتف الرئيسي',
-    'secondery_phone' => 'رقم الهاتف الثانوي ',
-    'address' => 'العنوان',
-    'side_slug' => 'Side Slug',
-    'social_link' => ' لينكات التواصل الإجتماعي',
-    'site_logo' => 'شعار الموقع ',
-    'message_maintenance' => 'رسالة الصيانة ',
-    'cancel'=>'إلغاء الأمر',
-    'save'=>'حفظ',
-    'site_icon'=>'رمز الموقع',
-    'facebook'=>'فيس بوك',
-    'twitter'=>'تويتر',
-    'inestergrame'=>'انستغرام',
-    'status'=>'حالة الموقع',
-
-
-
-    'email.required' => 'البريد الإلكتروني مطلوب',
-    'primary_phone.required' => 'الرقم الرئيسي للهاتف',
-    'secondery_phone.required' => 'الرقم الثانوي للهاتف',
-
-    'facebook.required' => 'عنوان الفيس بوك مطلوب',
-    'twitter.required' => 'عنوان التويتر مطلوب',
-    'inestegram.required' => 'عنوان الانستغرام مطلوب',
-
-    'site_name.required' => 'اسم الموقع مطلوب',
-    'status.required' => 'حالة الموقع مطلوبة',
-    'message_maintenance.required'=>'رسالة الصيانة مطلوبة',
-
-
-    'inestergrame'=>'انستغرام'
+    'page_title_in_sidebar'                                 =>               'الاعدادات العامة',
+    'settingPageTitle'                                      =>               'الاعدادات العامة',
+    'dashboard'                                             =>               'اللوحة الرئيسية',
+    'setting_info'                                          =>               'معلومات الإعدادات',
+    'settings'                                              =>               'الإعدادات',
+    'site_name'                                             =>               'إسم الموقع',
+    'supporting_mail'                                       =>               'البريد الإلكتروني',
+    'primary_phone'                                         =>               'رقم الهاتف الرئيسي',
+    'secondery_phone'                                       =>               'رقم الهاتف الثانوي ',
+    'address'                                               =>               'العنوان',
+    'site_logo'                                             =>               'شعار الموقع ',
+    'message_maintenance'                                   =>               'رسالة الصيانة ',
+    'cancel'                                                =>               'إلغاء الأمر',
+    'save'                                                  =>               'حفظ',
+    'site_icon'                                             =>               'رمز الموقع',
+    'facebook'                                              =>               'فيس بوك',
+    'twitter'                                               =>               'تويتر',
+    'inestergrame'                                          =>               'انستغرام',
+    'status'                                                =>               'حالة الموقع',
+    // validation ::
+    'email.required'                                        =>               'البريد الإلكتروني مطلوب',
+    'primary_phone.required'                                =>               'الرقم الرئيسي للهاتف',
+    'secondery_phone.required'                              =>               'الرقم الثانوي للهاتف',
+    'facebook.required'                                     =>               'عنوان الفيس بوك مطلوب',
+    'twitter.required'                                      =>               'عنوان التويتر مطلوب',
+    'inestegram.required'                                   =>               'عنوان الانستغرام مطلوب',
+    'site_name.required'                                    =>               'اسم الموقع مطلوب',
+    'status.required'                                       =>               'حالة الموقع مطلوبة',
+    'message_maintenance.required'                          =>              'رسالة الصيانة مطلوبة',
+    'inestergrame'                                          =>              'انستغرام'
 ];
