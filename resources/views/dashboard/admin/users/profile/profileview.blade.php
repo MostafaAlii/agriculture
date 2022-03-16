700f5b61547be960b88b029ed1d32746f587f2a1
wolf <ahmedragabyasin2020@gmail.com>
2022-03-16 22:42:23 +0200

    show button for all

--- resources/views/dashboard/admin/users/profile/profileview.blade.php
+++ resources/views/dashboard/admin/users/profile/profileview.blade.php
@@ -3,7 +3,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/notify/0.4.2/notify.min.js" integrity="sha512-efUTj3HdSPwWJ9gjfGR71X9cvsrthIA78/Fvd/IN+fttQVy7XWkOAXb295j8B3cmm/kFKVxjiNYzKw9IQJHIuQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 @endsection
 @section('pageTitle')
-    {{ trans('Admin/countries.countryPageTitle') }}
+     @lang('Admin/site.profile')
 @endsection
 
 @section('content')
@@ -20,24 +20,30 @@
                     <div class="col-12 col-sm-7">
                         <div class="media mb-2">
                             <a class="mr-1" href="#">
-                                <img src="../../../app-assets/images/portrait/small/avatar-s-26.png" alt="users view avatar" class="users-avatar-shadow rounded-circle" height="64" width="64">
+                                <img src="{{ asset('Dashboard/img/admins/'. $user->image->filename) }}"
+                                alt="{{ asset('Dashboard/img/admins/'. $user->image->filename) }}"
+                                class="users-avatar-shadow rounded-circle" height="64" width="64">
                             </a>
                             <div class="media-body pt-25">
-                                <h4 class="media-heading"><span class="users-view-name">Dean Stanley </span><span class="text-muted font-medium-1"> @</span><span class="users-view-username text-muted font-medium-1 ">candy007</span></h4>
-                                <span>ID:</span>
-                                <span class="users-view-id">305</span>
+                                <h4 class="media-heading">
+                                    <span class="users-view-name">{{ $user->firstname }} {{ $user->lastname }}</span>
+                                    {{-- <span class="text-muted font-medium-1"> @</span>
+                                    <span class="users-view-username text-muted font-medium-1 ">candy007</span> --}}
+                                </h4>
+                                {{-- <span>ID:</span> --}}
+                                <span class="users-view-id">{{ $user->email }}</span>
                             </div>
                         </div>
                     </div>
                     <div class="col-12 col-sm-5 px-0 d-flex justify-content-end align-items-center px-1 mb-2">
                         <a href="#" class="btn btn-sm mr-25 border"><i class="ft-message-square font-small-3"></i></a>
-                        <a href="#" class="btn btn-sm mr-25 border">Profile</a>
-                        <a href="../../../html/ltr/vertical-menu-template/page-users-edit.html" class="btn btn-sm btn-primary">Edit</a>
+                        {{-- <a href="#" class="btn btn-sm mr-25 border">Profile</a> --}}
+                        <a href="{{ route('profile.edit',encrypt($user->id)) }}" class="btn btn-sm btn-primary"> @lang('Admin/site.edit')</a>
                     </div>
                 </div>
                 <!-- users view media object ends -->
                 <!-- users view card data start -->
-                <div class="card">
+                {{-- <div class="card">
                     <div class="card-content">
                         <div class="card-body">
                             <div class="row">
@@ -108,13 +114,13 @@
                             </div>
                         </div>
                     </div>
-                </div>
+                </div> --}}
                 <!-- users view card data ends -->
                 <!-- users view card details start -->
                 <div class="card">
                     <div class="card-content">
                         <div class="card-body">
-                            <div class="row bg-primary bg-lighten-5 rounded mb-2 mx-25 text-center text-lg-left">
+                            {{-- <div class="row bg-primary bg-lighten-5 rounded mb-2 mx-25 text-center text-lg-left">
                                 <div class="col-12 col-sm-4 p-2">
                                     <h6 class="text-primary mb-0">Posts: <span class="font-large-1 align-middle">125</span></h6>
                                 </div>
@@ -124,30 +130,30 @@
                                 <div class="col-12 col-sm-4 p-2">
                                     <h6 class="text-primary mb-0">Following: <span class="font-large-1 align-middle">256</span></h6>
                                 </div>
-                            </div>
+                            </div> --}}
                             <div class="col-12">
                                 <table class="table table-borderless">
                                     <tbody>
                                         <tr>
-                                            <td>Username:</td>
-                                            <td class="users-view-username">dean3004</td>
+                                            <td>@lang('Admin/site.name'):</td>
+                                            <td class="users-view-username">{{ $user->firstname }} {{ $user->lastname }}</td>
                                         </tr>
-                                        <tr>
+                                        {{-- <tr>
                                             <td>Name:</td>
                                             <td class="users-view-name">Dean Stanley</td>
+                                        </tr> --}}
+                                        <tr>
+                                            <td>@lang('Admin/site.email'):</td>
+                                            <td class="users-view-email">{{ $user->email }}</td>
                                         </tr>
                                         <tr>
-                                            <td>E-mail:</td>
-                                            <td class="users-view-email">deanstanley@gmail.com</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Comapny:</td>
-                                            <td>XYZ Corp. Ltd.</td>
+                                            <td>@lang('Admin/site.phone'):</td>
+                                            <td>{{ $user->phone }}</td>
                                         </tr>
 
                                     </tbody>
                                 </table>
-                                <h5 class="mb-1"><i class="ft-link"></i> Social Links</h5>
+                                {{-- <h5 class="mb-1"><i class="ft-link"></i> Social Links</h5>
                                 <table class="table table-borderless">
                                     <tbody>
                                         <tr>
@@ -163,25 +169,45 @@
                                             <td><a href="#">https://www.instagram.com/</a></td>
                                         </tr>
                                     </tbody>
-                                </table>
-                                <h5 class="mb-1"><i class="ft-info"></i> Personal Info</h5>
+                                </table> --}}
+                                <h5 class="mb-1"><i class="ft-info"></i> @lang('Admin/site.personalinfo')</h5>
                                 <table class="table table-borderless mb-0">
                                     <tbody>
                                         <tr>
-                                            <td>Birthday:</td>
-                                            <td>03/04/1990</td>
+                                            <td>@lang('Admin/site.birthday'):</td>
+                                            <td>{{ $user->birthdate }}</td>
                                         </tr>
                                         <tr>
-                                            <td>Country:</td>
-                                            <td>USA</td>
+                                            <td>@lang('Admin/site.country'):</td>
+                                            <td>{{ $user->birthdate }}</td>
                                         </tr>
                                         <tr>
-                                            <td>Languages:</td>
-                                            <td>English</td>
+                                            <td>@lang('Admin/site.state'):</td>
+                                            <td>{{ $user->birthdate }}</td>
                                         </tr>
                                         <tr>
-                                            <td>Contact:</td>
-                                            <td>+(305) 254 24668</td>
+                                            <td>@lang('Admin/site.province'):</td>
+                                            <td>{{ $user->birthdate }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>@lang('Admin/site.village'):</td>
+                                            <td>{{ $user->birthdate }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>@lang('Admin/site.area'):</td>
+                                            <td>{{ $user->birthdate }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>@lang('Admin/site.department'):</td>
+                                            <td>{{ $user->birthdate }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>@lang('Admin/site.address1'):</td>
+                                            <td>{{ $user->address1 }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td>@lang('Admin/site.address2'):</td>
+                                            <td>{{ $user->address2 }}</td>
                                         </tr>
                                     </tbody>
                                 </table>
