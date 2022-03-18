5d9142b8e47909aa431b3d98507bd011b0648bf8
MostafaAlii <mostafa0alii@gmail.com>
2022-03-18 23:58:57 +0200

    provience

--- resources/views/dashboard/admin/countries/index.blade.php
+++ resources/views/dashboard/admin/countries/index.blade.php
@@ -125,8 +125,8 @@
         columns: [
             {data: 'record_select', name: 'record_select', searchable: false, sortable: false, width: '1%'},
              {data: 'image', name: 'country_logo', searchable: false, sortable: false, width: '10%'},
-            {data: 'name', name: 'name'},
-            {data: 'provinces', name: 'provinces.name'},
+            {data: 'name', name: 'name', searchable: false, sortable: false},
+            {data: 'provinces', name: 'provinces.name', searchable: false, sortable: false},
             {data: 'created_at', name: 'created_at', searchable: false},
             {data: 'actions', name: 'actions', searchable: false, sortable: false, width: '20%'},
         ],
--- resources/views/dashboard/admin/proviences/index.blade.php
+++ resources/views/dashboard/admin/proviences/index.blade.php
@@ -123,9 +123,9 @@
         },
         columns: [
             {data: 'record_select', name: 'record_select', searchable: false, sortable: false, width: '1%'},
-            {data: 'name', name: 'name'},
-            {data: 'country', name: 'country.name'},
-            {data: 'areas', name: 'areas.name'},
+            {data: 'name', name: 'name', searchable: false, sortable: false},
+            {data: 'country', name: 'country.name', searchable: false, sortable: false},
+            {data: 'areas', name: 'areas.name', searchable: false, sortable: false},
             {data: 'created_at', name: 'created_at', searchable: false},
             {data: 'actions', name: 'actions', searchable: false, sortable: false, width: '20%'},
         ],
