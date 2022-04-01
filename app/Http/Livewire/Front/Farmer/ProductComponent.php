3f423edfd25a2587a89a513fe0cfa3071829d67a
wolf <ahmedragabyasin2020@gmail.com>
2022-04-01 15:28:09 +0200

    farmer dash index 2

--- app/Http/Livewire/Front/Farmer/ProductComponent.php
+++ app/Http/Livewire/Front/Farmer/ProductComponent.php
@@ -10,7 +10,8 @@ class ProductComponent extends Component
     use WithPagination;
     public function render()
     {
-        $products = Product::paginate(5);
+        // $products = Product::paginate(5);
+        $products = Product::where( 'farmer_id',auth()->user()->id)->paginate(5);
         return view('livewire.front.farmer.product',compact('products'))
         ->layout('front.layouts.master2');
     }
--- resources/views/livewire/front/farmer/product.blade.php
+++ resources/views/livewire/front/farmer/product.blade.php
@@ -2,13 +2,14 @@
     <div class="container" style="padding: 30px 0;">
         <div class="row">
             <div class="col-md-12">
-                <h2>
-                    Hello from farmer product cat
-                </h2>
-               <h1> <a href="#" class="btn btn-primary"> <i class="fa fa-plus"></i>Add New Product</a></h1>
+               <h1> <a href="#" class="btn btn-primary btn-lg"> <i class="fa fa-plus"></i> Add New Product</a></h1>
                 <div class="panel panel-default">
                     <div class="panel-heading">
-                        <h5>All Product test</h5>
+                        <h5>All
+                            <span style="font-weight: bold;color: #33ab4e;">
+                              {{ Auth::user()->firstname .' ' . Auth::user()->lastname }}
+                            </span> Product
+                        </h5>
                     </div>
                     <div class="panel-body">
                         <table class="table table-striped">
@@ -25,19 +26,21 @@
                               </tr>
                             </thead>
                             <tbody>
+                                @if ($products->count() >0)
+
                                 @foreach ($products as $index=>$product )
                                     <tr>
                                         <th scope="row">{{ $index+1 }}</th>
                                         <td>
                                             @if($product->image->filename)
-                                            <a href="{{ route('product_details',$product->id) }}">
-                                                <img class="lazy" width="100"
-                                                     data-src="{{ asset('Dashboard/img/products/'. $product->image->filename) }}" alt="demo" />
-                                            </a>
-                                        @else
-                                            <img class="lazy" width="100" src="{{ asset('Dashboard/img/images/products/default.jpg') }}"
-                                            data-src="{{ asset('Dashboard/img/images/products/default.jpg') }}" alt="demo" />
-                                        @endif
+                                                <a href="{{ route('product_details',$product->id) }}">
+                                                    <img class="lazy" width="100"
+                                                        data-src="{{ asset('Dashboard/img/products/'. $product->image->filename) }}" alt="demo" />
+                                                </a>
+                                            @else
+                                                <img class="lazy" width="100" src="{{ asset('Dashboard/img/images/products/default.jpg') }}"
+                                                data-src="{{ asset('Dashboard/img/images/products/default.jpg') }}" alt="demo" />
+                                            @endif
                                         </td>
                                         <td>{{ $product->name }}</td>
                                         <td>
@@ -53,11 +56,15 @@
                                          </td>
                                         <td>{{ $product->created_at->diffforhumans() }} </td>
                                         <td>
-                                            <a href="" class="btn btn-success "><i class="fa fa-edit"></i> Edit</a>
-                                            <a href="" class="btn btn-warning "><i class="fa fa-trash"></i>Delete</a>
+                                            <a href="" class="btn btn-success btn-lg"><i class="fa fa-edit"></i> Edit</a>
+                                            <a href="" class="btn btn-warning btn-lg"><i class="fa fa-trash"></i>Delete</a>
                                         </td>
                                     </tr>
                               @endforeach
+
+                              @else
+                                 <h3 style="color: #e71d1d;">(There is no product to Show , please add new products !)</h3>
+                              @endif
                             </tbody>
                         </table>
                         @if (count($products))
