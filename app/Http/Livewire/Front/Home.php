1e4d0aa1d6508267ef618a760ea871f45d0b37ff
wolf <ahmedragabyasin2020@gmail.com>
2022-04-05 00:13:50 +0200

    cart migration for auth vendor

--- app/Http/Livewire/Front/Home.php
+++ app/Http/Livewire/Front/Home.php
@@ -5,6 +5,8 @@ namespace App\Http\Livewire\Front;
 use App\Models\Product;
 use Livewire\Component;
 use Cart;
+use Illuminate\Support\Facades\Auth;
+
 class Home extends Component
 {
     public function store($product_id,$product_name,$product_price){
@@ -18,6 +20,9 @@ class Home extends Component
         $newProducts = Product::latest()->limit(12)->get();
         $popProducts = Product::inRandomOrder()->get()->take(6);
         $saleProducts = Product::where('special_price','>',0)->latest()->get()->take(4);
+        if(Auth::guard('vendor')->check()){
+            Cart::instance('cart')->restore(Auth::guard('vendor')->user()->email);
+          }
         return view('livewire.front.home',compact('popProducts','saleProducts','newProducts'))
         ->layout('front.layouts.master1');
     }
