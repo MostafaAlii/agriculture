3ce6ce7bd0266a59355f0afa920ff6eab623b3b8
Mostafa Ali <57988145+MostafaAlii@users.noreply.github.com>
2022-04-08 23:50:45 +0200

    Update BlogSeeder.php

--- database/seeders/BlogSeeder.php
+++ database/seeders/BlogSeeder.php
@@ -1,11 +1,28 @@
 <?php
 namespace Database\Seeders;
+use App\Models\Tag;
 use App\Models\Blog;
-use Illuminate\Support\Facades\DB;
+use App\Models\Category;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 class BlogSeeder extends Seeder {
     public function run() {
-        DB::table('blogs')->delete();
-        Blog::factory(30)->create();
+        Schema::disableForeignKeyConstraints();
+        DB::table('blogs')->truncate();
+        Blog::factory()->count(30)->create();
+        $Tags = Tag::get();
+        Blog::all()->each(function ($blog) use ($Tags) {
+            $blog->tags()->attach(
+                $Tags->random()->pluck('id')->toArray()
+            );
+        });
+        $Categories = Category::get();
+        Blog::all()->each(function ($blog) use ($Categories) {
+            $blog->categories()->attach(
+                $Categories->random()->pluck('id')->toArray()
+            );
+        });
+        Schema::enableForeignKeyConstraints();
     }
 }
