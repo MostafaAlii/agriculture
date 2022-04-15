8335d691fe3ee5e63eb87113afd40e4fe94407c5
unknown <magdasaif3@gmail.com>
2022-04-15 23:10:20 +0200

    edit farmer_id to user_id

--- database/migrations/2022_04_09_232333_create_ratings_table.php
+++ database/migrations/2022_04_09_232333_create_ratings_table.php
@@ -7,7 +7,7 @@ class CreateRatingsTable extends Migration {
     public function up() {
         Schema::create('ratings', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('farmer_id')->nullable()->constrained('farmers', 'id')->cascadeOnDelete();
+            $table->foreignId('user_id')->nullable()->constrained('users', 'id')->cascadeOnDelete();
             $table->enum('rating', [Rating::MIN_STARS_RATE,2,3,4,Rating::MAX_STARS_RATE])->default(Rating::MIN_STARS_RATE);
             $table->morphs('rateable');
             $table->timestamps();
