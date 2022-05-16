833d218b0fc1c5acac2d67e459f35fa0a8d6ffe0
wolf <ahmedragabyasin2020@gmail.com>
2022-05-16 20:13:31 +0200

    _counter include in home front

--- resources/lang/ar/Admin/categories.php
+++ resources/lang/ar/Admin/categories.php
@@ -6,7 +6,7 @@ return [
     'depart_desc'                   =>'الوصف',
     'depart_keyword'                =>'الكلمات المفتاحيه',
 
-    
+
     'depart_type'                   =>'التصنيف',
     'depart_main'                   =>'رئيسى',
     'depart_sub'                    =>'فرعى',
@@ -15,12 +15,13 @@ return [
     'depart_select'                 =>'اختر النوع',
     'depart_main_type'              =>'قسم رئيسي',
     'depart_slug'                   =>'slug',
-  
+
     'depart_add_done'               =>'تم اضافه القسم بنجاح',
     'depart_edit_done'              =>'تم تعديل القسم بنجاح',
     'depart_delete_done'            =>'تم حذف القسم بنجاح',
     'depart_cant_delete'            =>'لا يمكن حذف هذا القسم لارتباطه بجداول اخرى',
-    
+
     'department'                    =>'الاداره',
+    'departments'                    =>'الادارات',
     'depart_choise'                 =>'اختر الاداره',
 ];
--- resources/lang/en/Admin/categories.php
+++ resources/lang/en/Admin/categories.php
@@ -6,7 +6,7 @@ return [
     'depart_desc'                   =>'Description',
     'depart_keyword'                =>'Keywords',
 
-    
+
     'depart_type'                   =>'Type',
     'depart_main'                   =>'Main',
     'depart_sub'                    =>'Sub',
@@ -15,12 +15,13 @@ return [
     'depart_select'                 =>'select type',
     'depart_main_type'              =>'Main Category',
     'depart_slug'                   =>'slug',
-    
+
     'depart_add_done'               =>'Category added sucessfully',
     'depart_edit_done'              =>'Category updated sucessfully',
     'depart_delete_done'            =>'Category deleted sucessfully',
     'depart_cant_delete'            =>'Can\'t delete this Category because it related with another tables',
 
+    'department'                    =>'Department',
     'department'                    =>'Departments',
     'depart_choise'                 =>'Choise Department',
 ];
