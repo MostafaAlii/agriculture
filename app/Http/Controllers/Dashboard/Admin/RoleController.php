86c00a2ce65513365a4a527f45f47bef4e8113d9
unknown <magdasaif3@gmail.com>
2022-05-20 02:26:35 +0200

    add store fun in role controller/interface

--- app/Http/Controllers/Dashboard/Admin/RoleController.php
+++ app/Http/Controllers/Dashboard/Admin/RoleController.php
@@ -26,7 +26,7 @@ class RoleController extends Controller {
     }
 
     public function store(Request $request) {
-        //
+        return $this->Data->store($request);
     }
 
     public function show($id) {
--- app/Http/Interfaces/Admin/RoleRepositoryInterface.php
+++ app/Http/Interfaces/Admin/RoleRepositoryInterface.php
@@ -4,4 +4,5 @@ interface RoleRepositoryInterface {
     public function index();
     public function data();
     public function create();
+    public function store($request);
 }
