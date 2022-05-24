c0d58d2293110040a672296393f4f8e6f5b64c9e
MostafaAlii <mostafa0alii@gmail.com>
2022-05-24 23:06:17 +0200

    fix roles edit blade

--- app/Http/Repositories/Admin/RoleRepository.php
+++ app/Http/Repositories/Admin/RoleRepository.php
@@ -51,7 +51,7 @@ class RoleRepository implements RoleRepositoryInterface {
         $real_id = Crypt::decrypt($id);
         $role = Role::find($real_id);
         $permission = Permission::get();
-        $rolePermissions = DB::table("role_has_permissions")->where("role_has_permissions.role_id",$id)
+        $rolePermissions = DB::table("role_has_permissions")->where("role_has_permissions.role_id",$real_id)
             ->pluck('role_has_permissions.permission_id','role_has_permissions.permission_id')
             ->all();
         return view('dashboard.admin.roles.edit',compact('role','permission','rolePermissions'));
