d6d0df2ae024a124b609b67c13eacdaed1bb3dad
unknown <magdasaif3@gmail.com>
2022-05-28 17:06:15 +0200

    change graph design in dash

--- resources/views/dashboard/admin/dashboard_index.blade.php
+++ resources/views/dashboard/admin/dashboard_index.blade.php
@@ -51,8 +51,8 @@
                             @csrf
 
                             <div class="row mt-2">
-                                <div class="col col-md-6">
-                                    <div class="form-group">
+                                <div class="row">
+                                    <div class="form-group col-md-3">
                                         <label for="area_id" style="float: right">{{ __('Admin/precipitations.area') }}</label>
                                         <select name="area_id" id="area_id" class="form-control"
                                                 required>
@@ -64,29 +64,29 @@
                                         </select>
 
                                     </div>
-                                    <div class="form-group">
+                                    <div class="form-group col-md-3">
                                         <h5 style="float: right">{{__('Admin\precipitations.from_date')}}<span class="text-danger"></span></h5>
                                         <div class="controls">
                                             <input type="date" name="start_date" id="start_date" class="form-control datepicker-autoclose" placeholder="Please select start date"> <div class="help-block"></div></div>
                                     </div>
-                                    <div class="form-group ">
+                                    <div class="form-group col-md-3">
                                         <h5 style="float: right">{{__('Admin\precipitations.to_date')}}<span class="text-danger"></span></h5>
                                         <div class="controls">
                                             <input type="date" name="end_date" id="end_date" class="form-control datepicker-autoclose" placeholder="Please select end date"> <div class="help-block"></div></div>
                                     </div>
+                                    <div class="form-group  col-md-3" >
+                                        <br>
+                                        <button type="submit" id="" class="btn btn-info">{{trans('Admin\Site.Get_chart')}}</button>
+                                    </div>
                                 </div>
-
-                                <div class="col col-md-6">
+                                <div class="col col-md-12">
                                     <div class="pie-chart-container">
                                         <canvas id="pie-chart" height="75"></canvas>
                                     </div>
                                 </div>
-
                             </div>
 
-                            <div class="text-left" style="    margin-left: 15px;  ">
-                                <button type="submit" id="" class="btn btn-info">{{trans('Admin\Site.Get_chart')}}</button>
-                            </div>
+                           
 
                         </form>
 
