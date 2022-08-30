4c97329890b779d6d189d0c768e9a8f4db4cfd5b
wolf <ahmedragabyasin2020@gmail.com>
2022-08-30 19:41:52 +0200

    fix team image

--- app/Http/Repositories/Admin/TeamRepository.php
+++ app/Http/Repositories/Admin/TeamRepository.php
@@ -7,6 +7,8 @@ use Yajra\DataTables\DataTables;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Crypt;
 use App\Http\Interfaces\Admin\TeamInterface;
+use Illuminate\Support\Str;
+use Intervention\Image\Facades\Image;
 
 class TeamRepository implements TeamInterface {
     use UploadT;
@@ -15,10 +17,10 @@ class TeamRepository implements TeamInterface {
     }
 //------------------------------------------------------------------------------------------
     public function data() {
-        
+
         // $team = Team::get();
         $team = Cache::get('teams');
-        
+
         return DataTables::of($team)
             ->addColumn('record_select', 'dashboard.admin.teams.data_table.record_select')
             ->editColumn('created_at', function (Team $team) {
@@ -37,34 +39,41 @@ class TeamRepository implements TeamInterface {
     }
 //------------------------------------------------------------------------------------------
     public function store($request) {
-    //  dd('gggg');
         try{
-             // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-             // Check img
-             if (!$request->file('image')->isValid()) {
-                flash('Invalid Image!')->error()->important();
-                return redirect()->back()->withInput();
+            //  if (!$request->file('image')->isValid()) {
+            //     flash('Invalid Image!')->error()->important();
+            //     return redirect()->back()->withInput();
+            // }
+            //  $photo = $request->image;
+            //  $filename =$request->image->getClientOriginalName();
+            //  $photo->storeAs('', $filename, 'team');
+            $input['name'] = $request->name;
+            $input['position'] = $request->position;
+            $input['description'] = $request->description;
+            if ($image = $request->file('image')) {
+                $file_name = Str::slug($request->name).".".$image->getClientOriginalExtension();
+                $path = public_path('/Dashboard/img/team/' . $file_name);
+                Image::make($image->getRealPath())->resize(500, null, function ($constraint) {
+                    $constraint->aspectRatio();
+                })->save($path, 100);
+                $input['image'] = $file_name;
             }
-             $photo = $request->image;
-             $filename =$request->image->getClientOriginalName();
-             $photo->storeAs('', $filename, 'team');
-             // &&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
-             
-            Team::Create(
-                [
-                    'name' => $request->name,
-                    'position' => $request->position,
-                    'description'=>$request->description,
-                    'image'=>$filename
-                ]
-            );
+            Team::create($input);
+            // Team::Create(
+            //     [
+            //         'name' => $request->name,
+            //         'position' => $request->position,
+            //         'description'=>$request->description,
+            //         'image'=>$filename
+            //     ]
+            // );
 
             //==============update cache file=================
             $t=Team::get();
             Cache::store('file')->put('teams',$t);
            //==================================================
             toastr()->success(__('Admin/attributes.added_done'));
-            return redirect()->route('team.index');   
+            return redirect()->route('team.index');
 
         } catch (\Exception $ex) {
              // dd($ex->getMessage());
@@ -83,43 +92,58 @@ class TeamRepository implements TeamInterface {
         try{
             $real_id = Crypt::decrypt($id);
             $team=Team::findOrfail($real_id);
-            $team->name=$request->name;
-            $team->position=$request->position;
-            $team->description=$request->description;
-
-            //============================================================================
-            if(isset($request->image)){
-                if($team->image) {//delete old image
-                    $old_photo = $team->image;
-                    $this->Delete_attachment('team', $old_photo, $team->id, $old_photo);
+            // $team->name=$request->name;
+            // $team->position=$request->position;
+            // $team->description=$request->description;
+            $input['name'] = $request->name;
+            $input['position'] = $request->position;
+            $input['description'] = $request->description;
+            if ($image = $request->file('image')) {
+                if($team->image != null && file_exists(public_path('/Dashboard/img/team/' . $team->image))){
+                    unlink('Dashboard/img/team/' . $team->image);
                 }
-                 // Check img
-                if (!$request->file('image')->isValid()) {
-                    flash('Invalid Image!')->error()->important();
-                    return redirect()->back()->withInput();
-                }
-
-                $photo = $request->image;
-                $filename =$request->image->getClientOriginalName();
-                $photo->storeAs('', $filename, 'team');
-                
-                $team->image=$filename;
+                $file_name = Str::slug($request->name).".".$image->getClientOriginalExtension();
+                $path = public_path('/Dashboard/img/team/' . $file_name);
+                Image::make($image->getRealPath())->resize(500, null, function ($constraint) {
+                    $constraint->aspectRatio();
+                })->save($path, 100);
+                $input['image'] = $file_name;
             }
-           //============================================================================
+            $team->update($input);
+            //============================================================================
+        //     if(isset($request->image)){
+        //         if($team->image) {//delete old image
+        //             $old_photo = $team->image;
+        //             $this->Delete_attachment('team', $old_photo, $team->id, $old_photo);
+        //         }
+        //          // Check img
+        //         if (!$request->file('image')->isValid()) {
+        //             flash('Invalid Image!')->error()->important();
+        //             return redirect()->back()->withInput();
+        //         }
 
-            $team->save();
+        //         $photo = $request->image;
+        //         $filename =$request->image->getClientOriginalName();
+        //         $photo->storeAs('', $filename, 'team');
+
+        //         $team->image=$filename;
+        //     }
+        //    //============================================================================
+
+        //     $team->save();
 
              //==============update cache file=================
              $t=Team::get();
              Cache::store('file')->put('teams',$t);
             //==================================================
-            
-            
+
+
             toastr()->success(__('Admin/attributes.updated_done'));
             return redirect()->route('team.index');
-        } catch (\Exception $ex) {
-            toastr()->success(__('Admin/attributes.edit_wrong'));
-            return redirect()->route('team.index');
+        } catch (\Exception $e) {
+            // toastr()->success(__('Admin/attributes.edit_wrong'));
+            // return redirect()->route('team.index');
+            return redirect()->back()->withErrors(['Error' => $e->getMessage()]);
          }
     }
 //------------------------------------------------------------------------------------------
@@ -128,23 +152,26 @@ class TeamRepository implements TeamInterface {
             $real_id = decrypt($id);
 
             $t= Team::findorfail($real_id);
-            unlink(public_path().'\Dashboard\img\team\\'.$t->image);
+            if($t->image != null && file_exists(public_path('/Dashboard/img/team/' . $t->image))){
+                unlink('Dashboard/img/team/' . $t->image);
+            }
+            // unlink(public_path().'\Dashboard\img\team\\'.$t->image);
             $t->delete();
 
             //==============update cache file=================
             $t=Team::get();
             Cache::store('file')->put('teams',$t);
            //==================================================
-           
+
                 toastr()->error(__('Admin/attributes.delete_done'));
                 return redirect()->route('team.index');
-           
+
         } catch (\Exception $e) {
             toastr()->success(__('Admin/attributes.delete_wrong'));
             return redirect()->route('team.index');
         }
     }
-    
+
 //------------------------------------------------------------------------------------------
     public function bulkDelete($request)
     {
@@ -153,11 +180,14 @@ class TeamRepository implements TeamInterface {
             $all_ids = explode(',',$request->delete_select_id);
 
             $delete_or_no=0;
-            
+
             foreach($all_ids as $ids){
-                
+
                     $t=Team::findorfail($ids);
-                    unlink(public_path().'\Dashboard\img\team\\'.$t->image);
+                    if($t->image != null && file_exists(public_path('/Dashboard/img/team/' . $t->image))){
+                        unlink('Dashboard/img/team/' . $t->image);
+                    }
+                    // unlink(public_path().'\Dashboard\img\team\\'.$t->image);
                     //$this->Delete_attachment('team', $t->image, $t->id, $t->image);
                     $t->delete();
                     $delete_or_no++;
@@ -170,7 +200,7 @@ class TeamRepository implements TeamInterface {
                     $t=Team::get();
                     Cache::store('file')->put('teams',$t);
                 //==================================================
-           
+
                 toastr()->error(__('Admin/attributes.delete_done'));
                 return redirect()->route('team.index');
             }
--- app/Http/Requests/Dashboard/TeamRequest.php
+++ app/Http/Requests/Dashboard/TeamRequest.php
@@ -5,28 +5,29 @@ class TeamRequest extends FormRequest {
     public function rules()
     {
         return [
-         'name'   => ['required', 'string', 'max:100','regex:/^[A-Za-z-أ-ي-pL\s\-0-9]+$/uu'],
-         'position'   => ['required', 'string', 'max:100','regex:/^[A-Za-z-أ-ي-pL\s\-0-9]+$/uu'],
-         'description'   => ['required', 'string', 'max:1000'],
-         'image'         =>'image|mimes:jpg,png,jpeg|dimensions:max_width=600,max_height=600',
+         'name'   => ['required', 'string', 'max:100'],
+         'position'   => ['required', 'string', 'max:100'],
+         'description'   => ['required', 'string'],
+        //  'image'         =>'image|mimes:jpg,png,jpeg|dimensions:max_width=600,max_height=600',
+         'image'         =>'image|mimes:jpg,png,jpeg',
 
         ];
     }
 
     public function messages() {
         return [
-            'name.required'         => trans('Admin/validation.required'),            
+            'name.required'         => trans('Admin/validation.required'),
             'name.max'              => trans('Admin/validation.max'),
             'name.string'           => trans('Admin/validation.string'),
             'name.regex'            => trans('Admin/validation.regex'),
-        
-            'position.required'         => trans('Admin/validation.required'),            
+
+            'position.required'         => trans('Admin/validation.required'),
             'position.max'              => trans('Admin/validation.max'),
             'position.string'           => trans('Admin/validation.string'),
             'position.regex'            => trans('Admin/validation.regex'),
-            
-            
-            'description.required'      => trans('Admin/validation.required'),            
+
+
+            'description.required'      => trans('Admin/validation.required'),
             'description.max'           => trans('Admin/validation.max'),
             'description.string'        => trans('Admin/validation.string'),
 
